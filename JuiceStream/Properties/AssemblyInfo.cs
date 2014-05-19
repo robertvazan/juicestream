3ba04fa00828a2271d1e2f73f3b95f3e23980236
Robert Vazan <robvcode@gmail.com>
2014-05-20 01:00:20 +0200

    NuGet package

--- JuiceStream/Properties/AssemblyInfo.cs
+++ JuiceStream/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JuiceStream")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generic Stream implementations: MultiplexStream, DuplexBufferedStream, PingStream, QueueStream, PrefixStream, CancellableStream. Particularly handy in binary protocols.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Robert Važan")]
 [assembly: AssemblyProduct("JuiceStream")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Robert Važan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
