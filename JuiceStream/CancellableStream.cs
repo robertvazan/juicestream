e671a6fdcce42cfdbe69178ece6dac6bd0f52489
Robert Vazan <robvcode@gmail.com>
2018-01-06 03:58:32 +0100

    Partial docs

--- JuiceStream/CancellableStream.cs
+++ JuiceStream/CancellableStream.cs
@@ -9,20 +9,60 @@ using System.Threading.Tasks;
 
 namespace JuiceStream
 {
+    /// <summary>
+    /// Enforces use of specified <see cref="T:System.Threading.CancellationToken"/> in all calls to the controlled <see cref="T:System.IO.Stream"/>.
+    /// </summary>
     public class CancellableStream : Stream
     {
         readonly Stream Inner;
         readonly CancellationToken Cancellation;
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:JuiceStream.CancellableStream"/> can read.
+        /// </summary>
+        /// <value><c>true</c> if can read; otherwise, <c>false</c>. Taken from wrapped stream.</value>
         public override bool CanRead { get { return Inner.CanRead; } }
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:JuiceStream.CancellableStream"/> can seek.
+        /// </summary>
+        /// <value><c>true</c> if can seek; otherwise, <c>false</c>. Taken from wrapped stream.</value>
         public override bool CanSeek { get { return Inner.CanSeek; } }
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:JuiceStream.CancellableStream"/> can timeout.
+        /// </summary>
+        /// <value><c>true</c> if can timeout; otherwise, <c>false</c>. Taken from wrapped stream.</value>
         public override bool CanTimeout { get { return Inner.CanTimeout; } }
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:JuiceStream.CancellableStream"/> can write.
+        /// </summary>
+        /// <value><c>true</c> if can write; otherwise, <c>false</c>. Taken from wrapped stream.</value>
         public override bool CanWrite { get { return Inner.CanWrite; } }
+        /// <summary>
+        /// Gets the length of the stream.
+        /// </summary>
+        /// <value>The length. Taken from wrapped stream.</value>
         public override long Length { get { return Inner.Length; } }
+        /// <summary>
+        /// Gets or sets the position in the stream.
+        /// </summary>
+        /// <value>The position. Taken from or written to the wrapped stream.</value>
         public override long Position { get { return Inner.Position; } set { Inner.Position = value; } }
+        /// <summary>
+        /// Gets or sets the read timeout.
+        /// </summary>
+        /// <value>The read timeout. Taken from or written to the wrapped stream.</value>
         public override int ReadTimeout { get { return Inner.ReadTimeout; } set { Inner.ReadTimeout = value; } }
+        /// <summary>
+        /// Gets or sets the write timeout.
+        /// </summary>
+        /// <value>The write timeout. Taken from or written to the wrapped stream.</value>
         public override int WriteTimeout { get { return Inner.WriteTimeout; } set { Inner.WriteTimeout = value; } }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:JuiceStream.CancellableStream"/> class.
+        /// </summary>
+        /// <param name="stream">Stream to wrap.</param>
+        /// <param name="cancellation">Cancellation token to enforce.</param>
         public CancellableStream(Stream stream, CancellationToken cancellation)
         {
             Inner = stream;
@@ -33,8 +73,35 @@ namespace JuiceStream
             if (disposing)
                 Inner.Dispose();
         }
+        /// <summary>
+        /// Begins asynchronous read from the stream.
+        /// Cancellation cannot be enforced for this method.
+        /// </summary>
+        /// <returns>Async operation status.</returns>
+        /// <param name="buffer">Target buffer.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to read.</param>
+        /// <param name="callback">Async callback for this operation.</param>
+        /// <param name="state">State to pass to the callback.</param>
         public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state) { return Inner.BeginRead(buffer, offset, count, callback, state); }
+        /// <summary>
+        /// Begins asynchronous write to the stream.
+        /// Cancellation cannot be enforced for this method.
+        /// </summary>
+        /// <returns>Async operation status.</returns>
+        /// <param name="buffer">Source buffer.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to write.</param>
+        /// <param name="callback">Async callback for this operation.</param>
+        /// <param name="state">State to pass to the callback.</param>
         public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state) { return Inner.BeginWrite(buffer, offset, count, callback, state); }
+        /// <summary>
+        /// Copies to another stream asynchronously.
+        /// </summary>
+        /// <returns>The to async.</returns>
+        /// <param name="destination">Destination.</param>
+        /// <param name="bufferSize">Buffer size.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public override async Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
         {
             if (cancellationToken == CancellationToken.None)
@@ -45,9 +112,28 @@ namespace JuiceStream
                     await Inner.CopyToAsync(destination, bufferSize, linked.Token);
             }
         }
+        /// <summary>
+        /// Completes asynchronous read.
+        /// </summary>
+        /// <returns>Bytes read.</returns>
+        /// <param name="asyncResult">Async result.</param>
         public override int EndRead(IAsyncResult asyncResult) { return Inner.EndRead(asyncResult); }
+        /// <summary>
+        /// Completes asynchronous write.
+        /// </summary>
+        /// <param name="asyncResult">Async result.</param>
         public override void EndWrite(IAsyncResult asyncResult) { Inner.EndWrite(asyncResult); }
+        /// <summary>
+        /// Flush this instance.
+        /// Synchronous operation. Cancellation cannot be enforced for this method.
+        /// </summary>
         public override void Flush() { Inner.Flush(); }
+        /// <summary>
+        /// Flush this instance asynchronously.
+        /// Cancellation can be enforced in addition to the passed cancellation token.
+        /// </summary>
+        /// <returns>Async task.</returns>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public override async Task FlushAsync(CancellationToken cancellationToken)
         {
             if (cancellationToken == CancellationToken.None)
@@ -58,7 +144,24 @@ namespace JuiceStream
                     await Inner.FlushAsync(linked.Token);
             }
         }
+        /// <summary>
+        /// Read data into the specified buffer at specified offset up to specified count.
+        /// Synchronous operation. Cancellation cannot be enforced for this method.
+        /// </summary>
+        /// <returns>Bytes actually read.</returns>
+        /// <param name="buffer">Buffer where data will be stored.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to read.</param>
         public override int Read(byte[] buffer, int offset, int count) { return Inner.Read(buffer, offset, count); }
+        /// <summary>
+        /// Read data asynchronously into the specified buffer at specified offset up to specified count.
+        /// Cancellation can be enforced in addition to the passed cancellation token.
+        /// </summary>
+        /// <returns>Async task.</returns>
+        /// <param name="buffer">Buffer where data will be stored.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to read.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
             if (cancellationToken == CancellationToken.None)
@@ -66,10 +169,41 @@ namespace JuiceStream
             using (var linked = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, Cancellation))
                 return await Inner.ReadAsync(buffer, offset, count, linked.Token);
         }
+        /// <summary>
+        /// Reads one byte from the stream.
+        /// Synchronous operation. Cancellation cannot be enforced for this method.
+        /// </summary>
+        /// <returns>The byte.</returns>
         public override int ReadByte() { return Inner.ReadByte(); }
+        /// <summary>
+        /// Seek to the specified offset relative to specified origin.
+        /// </summary>
+        /// <returns>The new position.</returns>
+        /// <param name="offset">Position relative to origin.</param>
+        /// <param name="origin">Origin where offset is counted from.</param>
         public override long Seek(long offset, SeekOrigin origin) { return Inner.Seek(offset, origin); }
+        /// <summary>
+        /// Sets the length of the stream.
+        /// </summary>
+        /// <param name="value">New length.</param>
         public override void SetLength(long value) { Inner.SetLength(value); }
+        /// <summary>
+        /// Write the specified buffer, from offset and up to count bytes.
+        /// Synchronous operation. Cancellation cannot be enforced for this method.
+        /// </summary>
+        /// <param name="buffer">Buffer with data to write.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to write.</param>
         public override void Write(byte[] buffer, int offset, int count) { Inner.Write(buffer, offset, count); }
+        /// <summary>
+        /// Write the specified buffer asynchronously, from offset and up to count bytes.
+        /// Cancellation can be enforced in addition to the passed cancellation token.
+        /// </summary>
+        /// <returns>Async task.</returns>
+        /// <param name="buffer">Buffer with data to write.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to write.</param>
+        /// <param name="token">Cancellation token.</param>
         public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
             if (cancellationToken == CancellationToken.None)
@@ -80,6 +214,11 @@ namespace JuiceStream
                     await Inner.WriteAsync(buffer, offset, count, linked.Token);
             }
         }
+        /// <summary>
+        /// Writes one byte.
+        /// Synchronous operation. Cancellation cannot be enforced for this method.
+        /// </summary>
+        /// <param name="value">Byte to write.</param>
         public override void WriteByte(byte value) { Inner.WriteByte(value); }
     }
 }
--- JuiceStream/QueueStream.cs
+++ JuiceStream/QueueStream.cs
@@ -10,6 +10,11 @@ using System.Threading.Tasks;
 
 namespace JuiceStream
 {
+    /// <summary>
+    /// <c>Stream</c> backed by in-memory buffer.
+    /// Writes to <c>QueueStream</c> come out as reads from the same <c>QueueStream</c>.
+    /// Only one write (of any length) is buffered before the stream starts blocking.
+    /// </summary>
     public class QueueStream : Stream
     {
         AsyncCollection<byte[]> Queue = new AsyncCollection<byte[]>(1);
@@ -17,13 +22,49 @@ namespace JuiceStream
         int ReadOffset;
         bool EndOfStream;
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:JuiceStream.QueueStream"/> can read.
+        /// Always <c>true</c>.
+        /// </summary>
+        /// <value><c>true</c> if can read; otherwise, <c>false</c>.</value>
         public override bool CanRead { get { return true; } }
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:JuiceStream.QueueStream"/> can seek.
+        /// Always <c>false</c>.
+        /// </summary>
+        /// <value><c>true</c> if can seek; otherwise, <c>false</c>.</value>
         public override bool CanSeek { get { return false; } }
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:JuiceStream.QueueStream"/> can timeout.
+        /// Always <c>false</c>.
+        /// </summary>
+        /// <value><c>true</c> if can timeout; otherwise, <c>false</c>.</value>
         public override bool CanTimeout { get { return false; } }
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:JuiceStream.QueueStream"/> can write.
+        /// Always <c>true</c>.
+        /// </summary>
+        /// <value><c>true</c> if can write; otherwise, <c>false</c>.</value>
         public override bool CanWrite { get { return true; } }
+        /// <summary>
+        /// Gets the length. Not supported. Throws <see cref="T:System.NotSupportedException"/>.
+        /// </summary>
+        /// <value>The length.</value>
         public override long Length { get { throw new NotSupportedException(); } }
+        /// <summary>
+        /// Gets or sets the position. Not supported. Throws <see cref="T:System.NotSupportedException"/>.
+        /// </summary>
+        /// <value>The position.</value>
         public override long Position { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+        /// <summary>
+        /// Gets or sets the read timeout. Not supported. Throws <see cref="T:System.NotSupportedException"/>.
+        /// </summary>
+        /// <value>The read timeout.</value>
         public override int ReadTimeout { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+        /// <summary>
+        /// Gets or sets the write timeout. Not supported. Throws <see cref="T:System.NotSupportedException"/>.
+        /// </summary>
+        /// <value>The write timeout.</value>
         public override int WriteTimeout { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
 
         protected override void Dispose(bool disposing)
@@ -33,9 +74,32 @@ namespace JuiceStream
             base.Dispose(disposing);
         }
 
+        /// <summary>
+        /// Flush this instance. It has no effect on <see cref="T:JuiceStream.QueueStream"/>.
+        /// </summary>
         public override void Flush() { }
+        /// <summary>
+        /// Flush this instance asynchronously. It has no effect on <see cref="T:JuiceStream.QueueStream"/>.
+        /// </summary>
+        /// <returns>Async task (already completed).</returns>
+        /// <param name="token">Cancellation token.</param>
         public override Task FlushAsync(CancellationToken token) { return TaskConstants.Completed; }
+        /// <summary>
+        /// Read data into the specified buffer at specified offset up to specified count.
+        /// </summary>
+        /// <returns>Bytes actually read.</returns>
+        /// <param name="buffer">Buffer where data will be stored.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to read.</param>
         public override int Read(byte[] buffer, int offset, int count) { return ReadAsync(buffer, offset, count).Result; }
+        /// <summary>
+        /// Read data asynchronously into the specified buffer at specified offset up to specified count.
+        /// </summary>
+        /// <returns>Async task.</returns>
+        /// <param name="buffer">Buffer where data will be stored.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to read.</param>
+        /// <param name="token">Cancellation token.</param>
         public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken token)
         {
             if (count > 0 && !EndOfStream)
@@ -57,9 +121,35 @@ namespace JuiceStream
             }
             return 0;
         }
+        /// <summary>
+        /// Seek to the specified offset relative to specified origin.
+        /// Not supported. Throws <see cref="T:System.NotSupportedException"/>.
+        /// </summary>
+        /// <returns>The new position.</returns>
+        /// <param name="offset">Position relative to origin.</param>
+        /// <param name="origin">Origin where offset is counted from.</param>
         public override long Seek(long offset, SeekOrigin origin) { throw new NotSupportedException(); }
+        /// <summary>
+        /// Sets the length of the stream.
+        /// Not supported. Throws <see cref="T:System.NotSupportedException"/>.
+        /// </summary>
+        /// <param name="value">New length.</param>
         public override void SetLength(long value) { throw new NotSupportedException(); }
+        /// <summary>
+        /// Write the specified buffer, from offset and up to count bytes.
+        /// </summary>
+        /// <param name="buffer">Buffer with data to write.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to write.</param>
         public override void Write(byte[] buffer, int offset, int count) { WriteAsync(buffer, offset, count).Wait(); }
+        /// <summary>
+        /// Write the specified buffer asynchronously, from offset and up to count bytes.
+        /// </summary>
+        /// <returns>Async task.</returns>
+        /// <param name="buffer">Buffer with data to write.</param>
+        /// <param name="offset">Offset into the buffer.</param>
+        /// <param name="count">Count of bytes to write.</param>
+        /// <param name="token">Cancellation token.</param>
         public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken token)
         {
             if (count != 0)
